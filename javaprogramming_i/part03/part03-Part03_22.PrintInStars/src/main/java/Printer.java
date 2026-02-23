6c566d1de28730fd9770d66679fd14c1303072b0
Kyrylo Kuznetsov <rizarkain@gmail.com>
2026-02-23 21:13:38 +0200

    Complete Part 3: Arrays section

---
+++ javaprogramming_i/part03/part03-Part03_22.PrintInStars/src/main/java/Printer.java
@@ -0,0 +1,25 @@
+
+public class Printer {
+
+    public static void main(String[] args) {
+        // You can test the method here
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayInStars(array);
+    }
+
+    public static void printArrayInStars(int[] array) {
+        // Write some code in here
+        for (int i = 0; i < array.length; i++) {
+           int number = array[i];
+           printStars(number);
+       }
+    }
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+}
+
+
